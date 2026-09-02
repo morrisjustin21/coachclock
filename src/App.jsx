3c4bc02340cc87d793e89af5e369e6a37a801864
morrisjustin21 <morris.justin21@gmail.com>
2026-09-01 21:43:19 -0500

    Add JoinRace route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import RaceList from './pages/RaceList'
 import RacePage from './pages/RacePage'
 import TeamRoster from './pages/TeamRoster'
+import JoinRace from './pages/JoinRace'
 
 export default function App() {
   const [session, setSession] = useState(null)
@@ -29,6 +30,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={session ? <RaceList session={session} /> : <Login />} />
       <Route path="/roster" element={session ? <TeamRoster session={session} /> : <Login />} />
+      <Route path="/join" element={session ? <JoinRace session={session} /> : <Login />} />
       <Route path="/race/:raceId" element={<RacePage session={session} />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
